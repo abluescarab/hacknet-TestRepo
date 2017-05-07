29efd27be1314c7bc66b091395aa9c99b292d61d
Alana Gilston <alana@abluescarab.us>
2017-05-06 23:47:35 -0700

    Initial commit

---
+++ Commands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Hacknet;
+
+namespace TemplateMod {
+    static class Commands {
+        public static class MyCommand {
+            public const string Key = "mycommand";
+            public const string Description = "This command does something";
+            public const string Usage = "Usage: " + Key;
+
+            public static bool RunCommand(OS os, List<string> args) {
+                return false;
+            }
+
+            public static bool LoadSettings(OS os, KeyValueFile file) {
+                return false;
+            }
+
+            public static bool SaveSettings(OS os, KeyValueFile file) {
+                return false;
+            }
+        }
+    }
+}
---
+++ TemplateMod.cs
@@ -0,0 +1,25 @@
+﻿using Pathfinder.Util;
+using Pathfinder.Command;
+
+namespace TemplateMod {
+    public class TemplateMod : Pathfinder.IPathfinderMod {
+        public const string Name = "";
+        public const string Version = "1_0";
+        public const string ID = Name + " v" + Version;
+        public const string Homepage = "https://github.com/username/repository";
+        public const string Author = "";
+        public string Identifier => ID;
+
+        public void Load() {
+            Logger.Verbose("Loading " + ID + "...");
+        }
+
+        public void LoadContent() {
+            Handler.AddCommand(Commands.MyCommand.Key, Commands.MyCommand.RunCommand, Commands.MyCommand.Description, true);
+        }
+
+        public void Unload() {
+            Logger.Verbose("Unloading " + ID + "...");
+        }
+    }
+}
